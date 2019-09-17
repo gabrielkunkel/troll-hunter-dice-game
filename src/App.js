13a2ab1144188b1f2f5a3908f816320c66f9b4ec
Gabriel Kunkel <henrysaltandson@gmail.com>
2019-09-17 14:45:34 -0500

    move logic into App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { gameplay } from './gameplay';
 import { MonsterFrame } from './MonsterFrame';
-
-let game = new gameplay();
+import { monsterDatabase } from './monsterDatabase';
 
 function App() {
+
+  const [monsters, updateMonsters] = useState(generateInitialMonsters());
+  const [player, updatePlayer] = useState({
+    level: 4,
+    life: 100
+  });
+
+  generateInitialMonsters();
+
+  /**
+   * @name generateInitialMonsters
+   * @returns {object[]} an array of monsters
+   */
+  function generateInitialMonsters() {
+    let monsters = [];
+
+    for (let i = 0; i < 3; i += 1) {
+      let whichMonster = rollDice(monsterDatabase.length) - 1;
+      let monsterToId = Object.assign({}, monsterDatabase[whichMonster], { id: i });
+      monsters.push(monsterToId);
+    }
+
+    return monsters;
+  }
+
+  /**
+   * @name rollDice
+   * @param {number} sides number of sides on dice
+   * @returns {number} roll of the dice
+   */
+  function rollDice(sides) {
+    return Math.floor(Math.random() * sides) + 1;
+  }
+
+  /**
+   * @name fight
+   * @param {number} monsterId 
+   */
+  function fight(monsterId) {
+
+    // roll
+    let monsterRoll = rollDice(monsters[monsterId].level);
+    let playerRoll = rollDice(player.level);
+
+    // compare and update 
+    if (monsterRoll > playerRoll) {
+      let playerLostLife = Object.assign({}, player, { life: player.life - (monsterRoll - playerRoll) });
+      updatePlayer(playerLostLife);
+      console.log("You lost that fight.");
+    } else if (monsterRoll < playerRoll) {
+      let MonstersUpdated = monsters.map(monster => {
+        if (monster.id === monsterId) {
+          return Object.assign({}, monster, { life: monsters[monsterId].life - (playerRoll - monsterRoll) });
+        } else {
+          return monster;
+        }
+      })
+      updateMonsters(MonstersUpdated);
+      console.log("You won that fight.");
+    }
+
+    // level up, win, or lose?
+    if (monsters[monsterId].life < 1) {
+      console.log("You killed the monster. Level up!");
+      let playerLeveledUp = Object.assign({}, player, { life: player.level + 1 });
+      updatePlayer(playerLeveledUp);
+      generateNewMonster(monsterId);
+    }
+
+    if (player.level === 8) {
+      console.log("You won.")
+    }
+
+    if (player.life < 1) {
+      console.log('You died. You\'re a loser')
+    }
+
+  }
+
+
+  function generateNewMonster(monsterId) {
+    let whichMonster = rollDice(monsterDatabase.length) - 1;
+    let newMonster = Object.assign({}, monsterDatabase[whichMonster], { id: monsterId });
+
+    let MonstersUpdated = monsters.map(monster => {
+      if (monster.id === monsterId) {
+        return newMonster;
+      } else {
+        return monster;
+      }
+    })
+
+    updateMonsters(MonstersUpdated);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,7 +107,7 @@ function App() {
       </header>
 
       <div className="monster-row">
-        {game.trolls.map(monster => <MonsterFrame key={monster.id} monster={monster} game={game} />)}
+        {monsters.map(monster => <MonsterFrame key={monster.id} monster={monster} fight={fight} />)}
       </div>
 
 
--- src/MonsterFrame.js
+++ src/MonsterFrame.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export const MonsterFrame = ({monster, game}) => {
+export const MonsterFrame = ({monster, fight}) => {
 
     return (
         <div>
-            <img src={monster.img} onClick={() => game.fight(monster.id)} alt={monster.name} />
+            <img src={monster.img} onClick={() => fight(monster.id)} alt={monster.name} />
             <i>{monster.name}</i>
         </div>
     )
