2393432dfcf33110bb2aba1fcc1e9e1901ae3877
Gabriel Kunkel <henrysaltandson@gmail.com>
2019-09-17 15:00:41 -0500

    make monster pictures appear

--- src/MonsterFrame.js
+++ src/MonsterFrame.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export const MonsterFrame = ({monster, fight}) => {
 
+    // <img src={require('/images/image-name.png')} />
+
     return (
         <div>
-            <img src={monster.img} onClick={() => fight(monster.id)} alt={monster.name} />
+            <img src={require(`/${monster.img}`)} onClick={() => fight(monster.id)} alt={monster.name} />
             <i>{monster.name}</i>
         </div>
     )
